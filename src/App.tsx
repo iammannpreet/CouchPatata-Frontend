a90dca968647739d7b04426f024b747ab8198116
iammannpreet <vickyarora.singh@gmail.com>
2025-01-26 20:04:59 -0500

    no progress not able to stack layers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import IntroLoader from './pages/introLoader/IntroLoader';
-import MovieList from './components/MovieList';
-import SmoothScrollWrapper from './common/scrollWrapper';
+import React, { useState, useEffect } from "react";
+import MovieList from "./components/MovieList";
+import AnimatedCounter from "./pages/introLoader/IntroLoader";
+import Index from "./components/Landing";
+import './App.css'
 
 const App: React.FC = () => {
   const [hasLoaded, setHasLoaded] = useState<boolean>(false);
@@ -9,20 +10,26 @@ const App: React.FC = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setHasLoaded(true);
-    }, 3000);
+    }, 3200);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <>
-      {!hasLoaded && <IntroLoader />}
-     
-     
-     <MovieList />
- 
-    </>
-
+    <div className="app-container relative">
+    {!hasLoaded ? (
+      <div className="relative h-screen bg-gray-900 text-white">
+        <div className="absolute bottom-[50px] left-[50px] text-9xl">
+          <AnimatedCounter from={0} to={100} />
+        </div>
+      </div>
+    ) : (
+      <div>
+        <Index />
+        <MovieList />
+      </div>
+    )}
+  </div>
   );
 };
 
